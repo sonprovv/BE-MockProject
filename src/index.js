21de31691c6057e338306fd585b5402c43b13344
tuva9 <tuva9@fpt.edu.vn>
2025-03-31 13:46:19 +0700

    update authencation middleware

--- src/index.js
+++ src/index.js
@@ -10,10 +10,19 @@ const middlewares = jsonServer.defaults();
 // /!\ Bind the router db to the app
 server.db = router.db;
 
+// Auth middleware
+const rules = auth.rewriter({
+  users: 600,
+  books: 664,
+  categories: 664,
+  products: 664
+})
+
 // Set a default port if PORT is undefined in the environment
 const PORT = process.env.PORT || 3000;
 
 server.use(middlewares);
+server.use(rules)
 server.use(auth)
 server.use(router);
 server.listen(PORT, () => {
